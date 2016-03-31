109bfe97a7729fce9e32de18de21e7bbb5bf795f
Harsh Goyal <hg060595@gmail.com>
2016-03-31 17:15:20 +0530

    Hello

---
+++ app.py
@@ -0,0 +1,66 @@
+from flask import Flask, render_template, request, session
+
+from src.common.database import Database
+from src.models.user import User
+
+app=Flask(__name__)
+app.secret_key="harsh"
+
+@app.route('/')
+def hello_method():
+    return render_template('home.html')
+
+@app.route('/login')
+def login_method():
+    return render_template('login.html')
+
+@app.route('/register')
+def register_method():
+    return render_template('register.html')
+
+@app.before_first_request
+def initialize_database():
+    Database.initialize()
+
+@app.route('/auth/login',methods=['POST'])
+def login_user():
+    email=request.form['email']
+    password=request.form['password']
+
+
+    if User.login_valid(email,password):
+        User.login(email)
+    else:
+        session['email']=None
+        print('Wrong Details')
+
+
+    return render_template("profile.html",email=session['email'])
+
+@app.route('/auth/register',methods=['POST'])
+def register_user():
+    email=request.form['email']
+    password=request.form['password']
+
+    User.register(email,password)
+
+    return render_template("profile.html",email=session['email'])
+
+
+@app.route('/blogs/<string:user_id>')
+@app.route('/blogs')
+def user_blogs(user_id=None):
+    if user_id is not None:
+
+        user=User.get_by_id(user_id)
+
+    else:
+        user=User.get_by_email(session['email'])
+
+    blogs=user.get_blogs()
+
+    return render_template("user_blogs.html",blogs=blogs,email=user.email)
+
+
+if __name__=='__main__':
+    app.run(debug=True)
